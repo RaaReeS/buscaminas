a98b1e15fb138dce25ae002bfedf0b62af5a8c42
Rares Stanescu <rares.stanescu00@gmail.com>
2025-12-17 13:14:52 +0100

    hecha clase casilla

--- src/buscaminas/Casilla.java
+++ src/buscaminas/Casilla.java
@@ -1,5 +1,66 @@
 package buscaminas;
 
 public class Casilla {
+    private boolean mina;
+    private boolean blanco;
+    private boolean bandera;
+    private boolean visible;
+    private int numero;
+
+    public Casilla() {
+    this.mina = false;
+    this.blanco = false;
+    this.bandera = false;
+    this.visible = false;
+    this.numero = 0;
+}
+
+
+    //Getters y setters
+    public boolean isMina() {
+        return mina;
+    }
+    public void setMina(boolean mina) {
+        this.mina = mina;
+    }
+    public boolean isBlanco() {
+        return blanco;
+    }
+    public void setBlanco(boolean blanco) {
+        this.blanco = blanco;
+    }
+    public boolean isBandera() {
+        return bandera;
+    }
+    public void setBandera(boolean bandera) {
+        this.bandera = bandera;
+    }
+    public boolean isVisible() {
+        return visible;
+    }
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String toString() {
+        if (isVisible()){
+            if (isBandera()) {
+                return "B";
+            } else if (isMina()) {
+                return "M";
+            } else if (isBlanco()) {
+                return " ";
+            } else {
+                return String.valueOf(getNumero());
+            }
+        }
+        return ".";
+    }
 
 }
