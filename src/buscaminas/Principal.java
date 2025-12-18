8a975aab9c44927139bc51c7877546de22d22e89
Alvaro Miguel <amigueldolz@gmail.com>
2025-12-18 09:42:18 +0100

    menu

--- src/buscaminas/Principal.java
+++ src/buscaminas/Principal.java
@@ -1,18 +1,15 @@
 package buscaminas;
 
-import java.util.Scanner;
-
+import entrada.Teclado;
 
 /**
  *
  * @author Alumno
  */
 
-
 public class Principal {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
         int opcion;
 
         do {
@@ -21,33 +18,33 @@ public class Principal {
             System.out.println("2-> Poner Bandera");
             System.out.println("3-> Quitar Bandera");
             System.out.print("Elige una opcion: ");
-            opcion = teclado.nextInt();
-            teclado.nextLine(); // limpiar buffer
+            opcion = Teclado.leerEntero("");
 
             switch (opcion) {
                 case 1:
                     System.out.println("Introduce la fila");
-                    int fila = teclado.nextLine();
+                    int fila = Teclado.leerEntero("");
 
                     System.out.println("Introduce la columna");
-                    int columna = teclado.nextLine();
-                    
+                    int columna = Teclado.leerEntero("");
+                    break;
 
                 case 2:
                     System.out.println("Introduce la fila");
-                    int fila = teclado.nextLine();
+                    fila = Teclado.leerEntero("");
 
                     System.out.println("Introduce la columna");
-                    int columna = teclado.nextLine();
+                    columna = Teclado.leerEntero("");
+                    break;
 
                 case 3:
                     System.out.println("Introduce la fila");
-                    int fila = teclado.nextLine();
+                    fila = Teclado.leerEntero("");
 
                     System.out.println("Introduce la columna");
-                    int columna = teclado.nextLine();
+                    columna = Teclado.leerEntero("");
+                    break;
             }
-        }
+        } while (opcion == 1 || opcion == 2 || opcion == 3);
     }
 }
-
